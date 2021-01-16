0743e38e2c2f39fb45e5ed3d71ccc8917bb35dba
İsmet Arslan <ismett.aslann@gmail.com>
2021-01-16 18:52:19 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
